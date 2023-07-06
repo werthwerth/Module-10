aa1e687a102085f035df04fe8e7f6fc58253032b
ivan.sakharov <teleperedozik@mail.ru>
2023-07-06 22:03:35 +0300

    ttuu

--- Module-10-5-1/Module-10-5-2.cs
+++ Module-10-5-1/Module-10-5-2.cs
@@ -2,80 +2,82 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-
-class Module_10_5_2
+namespace Module_10_5_2
 {
-    private static ILogger Logger { get; set; }
-
-    static void Main(string[] args)
+    class Module_10_5_2
     {
-        Logger = new Logger();
-        try
+        private static ILogger Logger { get; set; }
+
+        static void Main(string[] args)
         {
-            double Answer = 0;
-            ICalc calc = new Calc(Logger);
-            Console.WriteLine("Enrter number one: ");
-            double Num = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enrter number two: ");
-            double Num1 = Convert.ToDouble(Console.ReadLine());
-            Answer = calc.Sum(Num, Num1);
-            calc.Calculate(Answer);
+            Logger = new Logger();
+            try
+            {
+                double Answer = 0;
+                ICalc calc = new Calc(Logger);
+                Console.WriteLine("Enrter number one: ");
+                double Num = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enrter number two: ");
+                double Num1 = Convert.ToDouble(Console.ReadLine());
+                Answer = calc.Sum(Num, Num1);
+                calc.Calculate(Answer);
+            }
+            catch
+            {
+                Logger.Error("Wrong numbers!");
+            }
         }
-        catch
+    }
+
+    public interface ICalc
+    {
+        double Sum(double Num, double Num1);
+        void Calculate(double Answer);
+    }
+
+
+
+    public class Calc : ICalc
+    {
+        private ILogger Logger { get; }
+        public Calc(ILogger logger)
         {
-            Logger.Error("Wrong numbers!");
+            Logger = logger;
+        }
+        double ICalc.Sum(double Num, double Num1)
+        {
+            Logger.Event("Calculating...");
+            return Num + Num1;
+        }
+        void ICalc.Calculate(double Answer)
+        {
+            Console.WriteLine($"Result is: {Answer}");
+            Console.ReadLine();
         }
     }
-}
-
-public interface ICalc
-{
-    double Sum(double Num, double Num1);
-    void Calculate(double Answer);
-}
-
-
-
-public class Calc : ICalc
-{
-    private ILogger Logger { get; }
-    public Calc(ILogger logger)
+    public interface ILogger
     {
-        Logger = logger;
-    }
-    double ICalc.Sum(double Num, double Num1)
-    {
-        Logger.Event("Calculating...");
-        return Num + Num1;
-    }
-    void ICalc.Calculate(double Answer)
-    {
-        Console.WriteLine($"Result is: {Answer}");
-        Console.ReadLine();
-    }
-}
-public interface ILogger
-{
-    void Event(string Message);
+        void Event(string Message);
 
-    void Error(string Message);
-}
-
-public class Logger : ILogger
-{
-    public void Event(string Message)
-    {
-        Console.ForegroundColor = ConsoleColor.Blue;
-
-        Console.WriteLine(Message);
-        Console.ForegroundColor = ConsoleColor.White;
+        void Error(string Message);
     }
 
-    public void Error(string Message)
+    public class Logger : ILogger
     {
-        Console.ForegroundColor = ConsoleColor.Red;
+        public void Event(string Message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
 
-        Console.WriteLine(Message);
-        Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(Message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void Error(string Message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine(Message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
     }
 }
